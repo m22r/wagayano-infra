510841bf8c8dbac46c31e40db9d31b1059f20a76
m22r <h.matsuura116@gmail.com>
2019-10-30 15:07:40 +0900

    Add ci user

--- sequelcat/_outputs.tf
+++ sequelcat/_outputs.tf
@@ -4,3 +4,9 @@ output "command_endpoint" {
 output "interaction_endpoint" {
   value = "${module.api_gateway.endpoint_url}${local.interaction_path}"
 }
+output "aws_access_key_id_for_ci" {
+  value = aws_iam_access_key.ci.id
+}
+output "aws_secret_access_key_for_ci" {
+  value = aws_iam_access_key.ci.secret
+}
