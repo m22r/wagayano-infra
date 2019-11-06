01807ba22466afa9c0da8c9e7f185f65807abea3
m22r <h.matsuura116@gmail.com>
2019-11-06 15:35:03 +0900

    Add doberman_ci_user

--- test_db/_outputs.tf
+++ test_db/_outputs.tf
@@ -10,3 +10,9 @@ output "aws_access_key_id_for_rainbows_ci" {
 output "aws_secret_access_key_for_rainbows_ci" {
   value = module.rainbows_ci_user.secret
 }
+output "aws_access_key_id_for_doberman_ci" {
+  value = module.doberman_ci_user.id
+}
+output "aws_secret_access_key_for_doberman_ci" {
+  value = module.doberman_ci_user.secret
+}
