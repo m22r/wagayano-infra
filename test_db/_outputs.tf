06097621e24d84d6c9a837242a32e1c360d41b14
m22r <h.matsuura116@gmail.com>
2019-11-06 11:39:14 +0900

    Add rainbows_ci_user

--- test_db/_outputs.tf
+++ test_db/_outputs.tf
@@ -4,3 +4,9 @@ output "aws_access_key_id_for_phoenix_ci" {
 output "aws_secret_access_key_for_phoenix_ci" {
   value = module.phoenix_ci_user.secret
 }
+output "aws_access_key_id_for_rainbows_ci" {
+  value = module.rainbows_ci_user.id
+}
+output "aws_secret_access_key_for_rainbows_ci" {
+  value = module.rainbows_ci_user.secret
+}
